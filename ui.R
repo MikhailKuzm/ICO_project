15a8f2b34b78320528c71ec54c64c75578eb8a84
Mikhail <mikhailkuzmenkov95@gmail.com>
2020-10-25 12:23:10 +0300

    make selectBox

--- ui.R
+++ ui.R
@@ -1,24 +1,23 @@
-library(leaflet)
-library(shiny)
+
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
 
     # Application title
 titlePanel("Map"),
-    
-radioButtons("pal_but", label = h4("Раскрасить страны по:"),
+sidebarLayout(
+    sidebarPanel(
+        selectInput("pal_but", label = h4("Раскрасить страны по:"),
             choices = list("Регулирование Майнинга" = 1,
                          "Применение законодательства о цб" = 2, 
-                         "Свободное использование криптовалют" = 3), 
-                 selected = 1),
-    
+                         "Использование криптовалют в потребительских целях" = 3), 
+                 selected = 1)),
     
     mainPanel(
-        
         leafletOutput("mymap")
             )
     )
+)
 
 
         
