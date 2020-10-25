d0da4703f819bd353b93b234e2919e0ff9a055b1
Mikhail <mikhailkuzmenkov95@gmail.com>
2020-10-25 12:48:47 +0300

    make dashboard

--- ui.R
+++ ui.R
@@ -1,22 +1,22 @@
 
-
-# Define UI for application that draws a histogram
-ui <- fluidPage(
-
-    # Application title
-titlePanel("Map"),
-sidebarLayout(
-    sidebarPanel(
-        selectInput("pal_but", label = h4("Раскрасить страны по:"),
-            choices = list("Регулирование Майнинга" = 1,
-                         "Применение законодательства о цб" = 2, 
-                         "Использование криптовалют в потребительских целях" = 3), 
-                 selected = 1)),
-    
-    mainPanel(
-        leafletOutput("mymap")
+ui <- dashboardPage(
+    dashboardHeader(title = "Map"),
+    dashboardSidebar(
+        sidebarMenu(
+            menuItem("Map", tabName = "dashboard", icon = icon("dashboard"))
+        )),
+    dashboardBody(
+tabItem(tabName = "dashboard",
+         fluidRow(
+              box( selectInput("pal_but", label = h4("Раскрасить страны по:"),
+                                choices = list("Регулирование Майнинга" = 1,
+                                               "Применение законодательства о цб" = 2, 
+                                               "Использование криптовалют в потребительских целях" = 3), 
+                                selected = 1)),
+        box(leafletOutput("mymap"))
             )
     )
+)
 )
 
 
