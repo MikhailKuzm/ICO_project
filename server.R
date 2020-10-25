ec20e0ad5ea22a6e6c6814e92c974d08352fe4df
Mikhail <mikhailkuzmenkov95@gmail.com>
2020-10-25 12:20:21 +0300

    make labels

--- server.R
+++ server.R
@@ -1,12 +1,9 @@
-library(leaflet)
-library(shiny)
-library(htmltools)
+
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-        source("common_table.R", local = TRUE)
     
-    ####Define ifelse for color buttons####
+                        #### Define pal function for coloring  map####
 pal1 <- reactive({ 
     if (input$pal_but == 1) {
 
@@ -27,6 +24,7 @@ pal1 <- reactive({
  pal
 })
  
+                        #### Make argument for pal() function ####
 col_type <- reactive({
   if (input$pal_but == 1){
     x <- ico_common$mining_is_regulated
@@ -40,9 +38,30 @@ col_type <- reactive({
   x
 })
 
+                        #### Make interactive labels for map ####
+labels <- reactive ({ 
+  if (input$pal_but == 1){
+  paste ("<p>", ico_common$country, "</p>",
+                 "<p>", "Регулирование Майнинга: ",
+                 ico_common$mining_is_regulated, "</p>",
+                 sep = "")
+  }else{
+     if (input$pal_but == 2){
+      paste ("<p>", ico_common$country, "</p>",
+             "<p>", "Применение законодательства о цб: ",
+             ico_common$direct_application_of_securities_legislation, "</p>",
+             sep = "")
+     } else {
+       paste ("<p>", ico_common$country, "</p>",
+              "<p>", "Использование криптовалют в потребительских целях: ",
+              ico_common$payment_for_goods_and_services_in_cryptocurrency, "</p>",
+              sep = "")
+     }
+  }
+})
 
-   #temp <- pal1()
-    #### Draw map ####
+
+                        #### Draw map ####
     output$mymap <- renderLeaflet({
         leaflet() %>%
             addPolygons(data = my_shape, 
@@ -55,7 +74,9 @@ col_type <- reactive({
                             weight = 2,
                             color = "red",
                             fillOpacity = 0.4,
-                            bringToFront = FALSE))
+                            bringToFront = FALSE),
+                        label = lapply(labels(), HTML))
 
     })
+
 }
