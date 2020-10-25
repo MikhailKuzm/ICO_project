61e699ec74a0a60239dbfa389223e774b2a0a349
Mikhail <mikhailkuzmenkov95@gmail.com>
2020-10-25 13:33:57 +0300

    make menuSubitem for map

--- ui.R
+++ ui.R
@@ -2,18 +2,22 @@
 ui <- dashboardPage(
     dashboardHeader(title = "Map"),
     dashboardSidebar(
+        width = 250,
         sidebarMenu(
-            menuItem("Map", tabName = "dashboard", icon = icon("dashboard"),
-            selectInput("pal_but", label = h4("Раскрасить страны по:"),
+            menuItem("Map", tabName = "dashboard", icon = icon("th"),
+                     menuSubItem(icon = NULL,
+                                 selectInput("pal_but", label = h5("Раскрасить страны по:"),
                                  choices = list("Регулирование Майнинга" = 1,
                                                 "Применение законодательства о цб" = 2, 
                                                 "Использование криптовалют в потребительских целях" = 3), 
                                  selected = 1))
-        )),
+        ))),
     dashboardBody(
 tabItem(tabName = "dashboard",
          fluidRow(
-        box(leafletOutput("mymap"))
+        box(leafletOutput("mymap"),
+            width = 12,
+            height = "380px")
             )
     )
 )
