e549da8bf859b4e739989ab610a73a1b8f826e1e
Mikhail <mikhailkuzmenkov95@gmail.com>
2020-10-26 14:39:18 +0300

    make text under map (observeEvent)

--- server.R
+++ server.R
@@ -64,19 +64,57 @@ labels <- reactive ({
                         #### Draw map ####
     output$mymap <- renderLeaflet({
         leaflet() %>%
+        setView(lat=52, lng=34 , zoom=2) %>%
+        addTiles() %>% 
             addPolygons(data = my_shape, 
-                        weight = 1,
+                        fill = "white",
+                        stroke=TRUE, 
+                        weight = 0.3,
                         smoothFactor = 0.5,
-                        color = pal1()(col_type()),
-                        fillOpacity = 0.8,
+                        color = 'white',
+                        fillOpacity = 0.2,
                         fillColor = pal1()(col_type()),
                         highlight = highlightOptions(
                             weight = 2,
                             color = "red",
-                            fillOpacity = 0.4,
+                            fillOpacity = 0.8,
                             bringToFront = FALSE),
-                        label = lapply(labels(), HTML))
+                        label = lapply(labels(), HTML),
+                        labelOptions = labelOptions( 
+                          style = list("font-weight" = "normal", padding = "3px 8px"), 
+                          textsize = "13px", 
+                          direction = "auto"),
+                        layerId = ~NAME)
 
+    })
+    
+                  #### Make text under map (based on click on map) ####
+ observeEvent(input$mymap_shape_click, {
+  p <- input$mymap_shape_click
+  information <- paste("<p>","<b>","Страна:","</b>", p$id,"</p>",
+                       "<p>", "<b>","Название утилити токена:","</b>", 
+                        ico_common[ico_common$country == p$id, "name_utility"], "</p>",
+                        "<p>", "<b>","Название криптовалют:", "</b>",
+                        ico_common[ico_common$country == p$id, "name_cryptocurrency"], "</p>",
+                        "<p>", "<b>","Название токенов-активов:","</b>",
+                        ico_common[ico_common$country == p$id, "name_assets"], "</p>",
+                           "<p>", "<b>","Определение утилити токена:","</b>",
+                        ico_common[ico_common$country == p$id, "definition_utility"],"</p>", 
+                       "<p>", "<b>","Определение криптовалют:","</b>",
+                        ico_common[ico_common$country == p$id, "definition_cryptocurrency"], "</p>", 
+                       "<p>",  "<b>","Определение токенов-активов:","</b>",
+                       ico_common[ico_common$country == p$id, "definition_assets"], "</p>"
+                       )
+      
+      information <- lapply(information, HTML)
+      
+   output$text <- renderUI({
+     information 
+         })
     })
 
 }
+
+
+
+
