3c7b8b9ac15be2f4a4b10ee40c286c8d35a76320
Mikhail <mikhailkuzmenkov95@gmail.com>
2020-10-26 14:49:50 +0300

    correct "source"

--- global.R
+++ global.R
@@ -5,9 +5,5 @@ library(leaflet)
 library(shiny)
 library(shinydashboard)
 
-<<<<<<< HEAD
-
-source("common_table.R", local = TRUE)
-=======
 source("common_table.R")
->>>>>>> 0ae2ad612a92967debafa379392fe84f2666dfe1
+
