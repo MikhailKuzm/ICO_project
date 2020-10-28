a6d643c8cf3dfce4975b17f608972cda88d54d4a
Mikhail <mikhailkuzmenkov95@gmail.com>
2020-10-28 19:46:48 +0300

    make Russian names in column

--- common_table.R
+++ common_table.R
@@ -1,17 +1,15 @@
-library(leaflet)
 library(rgdal)
 library(dplyr)
-library(htmltools)
 library(readxl)
 
-my_shape <- readOGR(dsn = "./ne_10m_admin_0_countries_lakes", 
-                    layer = "ne_10m_admin_0_countries_lakes")
+
+my_shape <- readRDS("my_shape")
 ico_common <- read_excel("./ico_project.xlsx", sheet = 1)
 
 ###make a table with all country names as in shape file
 
-temp <- match(ico_common$Country, my_shape$NAME)
-names <- my_shape$NAME[-temp]
+temp <- match(ico_common$Country, my_shape$NAME_RU)
+names <- my_shape$NAME_RU[-temp]
 Country <- ico_common$Country
 x <- ico_common
 ### create table with all countries
@@ -24,8 +22,9 @@ new_table <- full_join(x, Country1, by = "Country")
 
 
 ###match shape file names and new table names
-ico_common <- new_table[order(match(new_table$Country, my_shape$NAME)),]
+ico_common <- new_table[order(match(new_table$Country, my_shape$NAME_RU)),]
 names(ico_common) <-tolower(names(ico_common))
+ico_common <- as.data.frame(ico_common)
 remove(new_table, x, names, temp, Country1)
                                             
 
