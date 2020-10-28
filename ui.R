c79c3b5097f4f4d0702e1b1781680e6320672a94
Mikhail <mikhailkuzmenkov95@gmail.com>
2020-10-28 23:02:37 +0300

    country name in the center

--- ui.R
+++ ui.R
@@ -4,24 +4,29 @@ ui <- dashboardPage(skin = "green",
     dashboardSidebar(
         width = 240,
         sidebarMenu(
-            menuItem("Map", tabName = "dashboard", icon = icon("th"),
+            menuItem("Карта", tabName = "dashboard", icon = icon("th"),
                      menuSubItem(icon = NULL,
                                  selectInput("pal_but", label = h5("Раскрасить страны по:"),
                                  choices = list("Регулирование Майнинга" = 1,
                                                 "Применение законодательства о цб" = 2, 
                                                 "Использование криптовалют в потребительских целях" = 3), 
                                  selected = 1))
-        ))),
+        )
+        
+        )),
     dashboardBody(
 tabItem(tabName = "dashboard",
-         fluidRow(
+        fluidRow(
         box(leafletOutput("mymap", height=380),
             width = 12,
             height = "400px"),
-        box(uiOutput("text"),
+        fluidPage(align = "center",
+        uiOutput("text1")),
+        box(uiOutput("text2"),
+            background = "light-blue",
             width = 12)
-            )
-    )
+)
+)
 )
 )
 
