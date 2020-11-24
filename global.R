71cb47cf18b6bb0798705c4bfbf931d44bf63969
Mikhail Kuzmenkov <63548200+MikhailKuzm@users.noreply.github.com>
2020-11-24 14:07:07 +0300

    new options

--- global.R
+++ global.R
@@ -1,3 +1,4 @@
+options(spinner.color="#0275D8", spinner.color.background="#ffffff", spinner.size=2)
 library(leaflet)
 library(shiny)
 library(htmltools)
@@ -5,6 +6,8 @@ library(leaflet)
 library(shiny)
 library(shinydashboard)
 library(shinyjs)
+library(shinydashboardPlus)
+library(shinycssloaders)
 
 source("common_table.R")
 
